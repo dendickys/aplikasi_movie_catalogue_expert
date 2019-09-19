eacf9b6352a99490a20819d7bc7c1c46e4e58834
dendickys <febri.unquad2014@gmail.com>
2019-09-20 00:41:58 +0700

    Added SplashActivity

---
+++ app/src/main/java/com/dendickys/aplikasimoviecatalogue/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.dendickys.aplikasimoviecatalogue;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+                    sleep(2400);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    finish();
+                }
+            }
+        };
+        thread.start();
+    }
+}
